4acd411ed0ec769e8cd1812dfad7a7b870d97d57
Dusan Jovanovic <jovanovd@uoguelph.ca>
2025-11-17 12:53:59 -0500

    Reworked student and domain JSON file format, as well as updating function stubs in main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -32,50 +32,52 @@ def create_new_student_file(name, email):
     """ Create new student profile locally. 
         Used when new student registers. """
     new_student = {
-        "New_Student": {
         "name": name,
         "email": email,
         "mastery": {
-            "Algebra Basics":{
-                "Overall Unit 1": 0.0,
-                "Section 1.1": 0.0,
-                "Section 1.2": 0.0,
-                "Section 1.3": 0.0
-            } ,
-            "Quadratic Equations":{
-                "Overall Unit 2": 0.0,
-                "Section 2.1": 0.0,
-                "Section 2.2": 0.0,
-                "Section 2.3": 0.0
-            } ,
-            "Geometry": {
-                "Overall Unit 3": 0.0,
-                "Section 3.1": 0.0,
-                "Section 3.2": 0.0,
-                "Section 3.3": 0.0
-            },
-            "Exponentials and Radicals": {
-                "Overall Unit 4": 0.0,
-                "Section 4.1": 0.0,
-                "Section 4.2": 0.0,
-                "Section 4.3": 0.0
+        "algebra_basics": {
+            "sections": {
+                "1": 0.0,
+                "2": 0.0,
+                "3": 0.0
             }
         },
-        "metrics": {
-            "total_questions_answered": 0,
-            "correct_answers": 0,
-            "incorrect_answers": 0,
-            "average_time_per_question": 0
+        "quadratic_equations": {
+            "sections": {
+                "1": 0.0,
+                "2": 0.0,
+                "3": 0.0
+            }
         },
-        "badges": [],
-        "question_history": [],
-        "quiz_history":[],
-        "current_session": {
-            "question_id": None,
-            "hints_used": 0,
-            "start_time": None
+        "geometry": {
+            "sections": {
+                "1": 0.0,
+                "2": 0.0,
+                "3": 0.0
+            }
+        },
+        "exponentials_radicals": {
+            "sections": {
+                "1": 0.0,
+                "2": 0.0,
+                "3": 0.0
+            }
         }
-        }
+    },
+    "metrics": {
+        "total_questions_answered": 0,
+        "correct_answers": 0,
+        "incorrect_answers": 0,
+        "average_time_per_question_ms": 0
+    },
+    "badges": [],
+    "question_history": [],
+    "quiz_history": [],
+    "current_session": {
+        "question_id": None,
+        "hints_used": 0,
+        "start_time": None
+    }
     }
     file_path = os.path.join(DATA_DIR, f"student_{name}.json")
     with open(file_path, "w", encoding="utf-8") as f:
@@ -96,6 +98,11 @@ def update_mastery(student, unit_name, section_name, new_score):
         Used after student answers question, or for each question when quiz is graded. """
     return
 
+def calculate_overall_unit_mastery(student, unit_name):
+    """ Recalculates overall unit mastery. 
+        Used when student does a quiz or practice for a specific section. """
+    return
+
 def generate_question(student, unit_name, section_name):
     """ Generate question based on student information and topic.
         Used for practice mode, or when generating quiz. """
@@ -107,11 +114,23 @@ def record_current_question(student):
         in current session information. """
     return
     
-def grade_question(student, question_id, submitted_answer,):
+def grade_question(student, question_id, submitted_answer):
     """ Compares sumitted answer with correct answer.
         Used after student submits questions, returns correctness and feedback, and
         updates student mastery, question history, and student metrics.
         Takes note of response time and saves results for student metrics and question generation. """
+    return
+
+def start_practice_session(student, unit_name, section_name):
+    """ Begins a practice session by generating a question, and 
+        initializes current session details in file. 
+        Used when student starts a practice session for a unit/section. """
+    return
+
+def end_practice_session(student):
+    """ Ends practice session, removes current session details from
+        student file, records time of session and recalculates overall mastery. 
+        Used when student exits a practice session. """
     return
 
 def generate_quiz(student, unit_name, section_name):
@@ -139,7 +158,6 @@ def answer_question():
 @app.route('/generate_quiz', methods=['GET'])
 def generate_quiz():
     return
-
 
 
 if __name__ == "__main__":
